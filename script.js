ab7ca1bc4bfd2c7b0f860d3b04cf80acd576b181
Kevin Irias <kiriasru@gmail.com>
2023-09-10 19:23:21 -0600

    Se han agregado index, script y style y se ha agregado una lista de contactos con una funcion para agregar contactos nuevos

---
+++ script.js
@@ -0,0 +1,5 @@
+const listaDeContactos = ["Sofia Lopez", "Bruno Romero", "Yury Garcia", "Marissa Perdomo"];
+
+function agregarContacto(nuevoContacto) {
+    listaDeContactos.push(nuevoContacto);
+}
