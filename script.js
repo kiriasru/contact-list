3b3886b42bd620f07828e4796bf31dcc778c2323
Kevin Irias <kiriasru@gmail.com>
2023-09-14 16:22:35 -0600

    Se han agregado 3 contactos

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-const listaDeContactos = [
-    "Sofia Lopez",
-    "Bruno Romero",
-    "Yury Garcia",
-    "Marissa Perdomo"
-];
+const contacto1 = {
+  id: 1,
+  nombres: 'Sofia Nicole',
+  apellidos: 'Lopez Almendares',
+  telefono: '98203350',
+  ubicaciones: {
+    ciudad: 'Tegucigalpa',
+    direccion: 'Residencial Arturo Quesada'
+  },
+};
 
-function agregarContacto(nuevoContacto) {
-    listaDeContactos.push(nuevoContacto);
+const contacto2 = {
+  id: 2,
+  nombres: 'Daniela Raquel',
+  apellidos: 'Gogel Aguilar',
+  telefono: '33104870',
+  ubicaciones: {
+    ciudad: 'Tegucigala',
+    direccion: 'Residencial La Cañada'
+  },
+};
+
+const contacto3 = {
+  id: 3,
+  nombres: 'Daniela Raquel',
+  apellidos: 'Gogel Aguilar',
+  telefono: '33104870',
+  ubicaciones: {
+    ciudad: 'Tegucigala',
+    direccion: 'Residencial La Cañada'
+  },
+};
+
+// Función para crear un contacto
+function crearContacto(id, nombres, apellidos, telefono, ciudad, direccion) {
+  const nuevoContacto = Object.create(Contacto); // Crea un nuevo objeto basado en la estructura de Contacto
+  nuevoContacto.id = id;
+  nuevoContacto.nombres = nombres;
+  nuevoContacto.apellidos = apellidos;
+  nuevoContacto.telefono = telefono;
+  nuevoContacto.ubicaciones.ciudad = ciudad;
+  nuevoContacto.ubicaciones.direccion = direccion;
+
+  // Devuelve el nuevo objeto de contacto
+  return nuevoContacto;
 }
 
-agregarContacto("Shady Binaeli");
+// Supongamos que tienes una lista de contactos
+const listaDeContactos = [];
 
-function quitarContacto(removerContacto) {
-    const posicion = listaDeContactos.indexOf(removerContacto);
-    if (posicion !== -1) {
-        listaDeContactos.splice(posicion, 1);
-    } 
+// Función para eliminar un contacto
+function eliminarContacto(contacto) {
+  const index = listaDeContactos.indexOf(contacto);
+  if (index !== -1) {
+    listaDeContactos.splice(index, 1); // Elimina el contacto de la lista
+  }
 }
-
-quitarContacto("Bruno Romero");
-
-function imprimirContactos() {
-    for (let i = 0; i < listaDeContactos.length; i++) {
-        console.log(listaDeContactos[i]);
-    }
-}
-
-imprimirContactos();
-
 
